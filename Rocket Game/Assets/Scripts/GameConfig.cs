1638368acc5aff3bee344675d7425a0c797a2a72
Vitaliy Prikota <mr.prikota@gmail.com>
2020-01-23 20:34:46 +0300

    Turned off vsync

---
+++ Rocket Game/Assets/Scripts/GameConfig.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameConfig : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Application.targetFrameRate = -1;
+        QualitySettings.vSyncCount = 0;
+    }
+
+
+}
