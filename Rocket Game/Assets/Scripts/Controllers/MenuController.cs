c904a3575db87b40792b93a9b23786e88b0aa0b4
Vitaliy Prikota <mr.prikota@gmail.com>
2020-01-26 23:21:08 +0300

    Finally made scene transiion smooth. Ye boah

--- Rocket Game/Assets/Scripts/Controllers/MenuController.cs
+++ Rocket Game/Assets/Scripts/Controllers/MenuController.cs
@@ -1,13 +1,25 @@
-﻿using UnityEngine;
-using TMPro;
+﻿using TMPro;
+using UnityEngine;
 public class MenuController : MonoBehaviour {
     [SerializeField] TextMeshProUGUI highscoreLabel;
-
-    void Start() {
+    [SerializeField] private LevelLoader levelLoader;
+    public void Start() {
         PlayerData data = SaveSystem.LoadData();
         if (data == null)
             highscoreLabel.text += " 0";
         else
             highscoreLabel.text += " " + data.highscore.ToString();
+    }
+
+    public void OnStartClick() {
+        Debug.Log("Clicked start");
+        levelLoader.LoadGameScene();
+    }
+
+    public void OnStartRelease() {
+    }
+
+    public void OnExitClick() {
+        Application.Quit();
     }
 }
---
+++ Rocket Game/Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class LevelLoader : MonoBehaviour {
+    public Animator transition;
+    public float transitionTime = 1f;
+    public void LoadGameScene() {
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+    }
+
+    IEnumerator LoadLevel(int sceneIndex) {
+        Debug.Log("Started animation");
+        transition.SetTrigger("Start");
+        yield return new WaitForSeconds(transitionTime);
+
+        switch (sceneIndex) {
+            case 0:
+                SceneLoader.LoadStartScene();
+                break;
+            case 1:
+                SceneLoader.LoadGameScene();
+                break;
+        }
+
+    }
+}
